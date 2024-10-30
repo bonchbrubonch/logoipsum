aadddb59ed9fd657b7764c50ea064974618082dd
bonchbrubonch <58067420+bonchbrubonch@users.noreply.github.com>
2024-10-30 17:14:02 +0200

    add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -8,12 +8,22 @@ $(function () {
 })
 
 var swiper = new Swiper(".partners__slider", {
-	slidesPerView: 5,
+	slidesPerView: 2,
 	spaceBetween: 25,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
+	breakpoints: {
+		576: {
+			slidesPerView: 4,
+			spaceBetween: 20,
+		},
+		992: {
+			slidesPerView: 5,
+			spaceBetween: 20,
+		},
+	}
 });
 
 $(".accordeon dd").hide().prev().click(function () {
@@ -22,3 +32,28 @@ $(".accordeon dd").hide().prev().click(function () {
 	$("dl").removeClass("open");
 	$(this).parent().toggleClass("open");
 });
+
+
+ // меню гамбургер
+ const menuBtn = document.querySelector(".header__menu-btn");
+ const navMenu = document.querySelector(".header__nav");
+ const header = document.querySelector(".header");
+ 
+ if (menuBtn && navMenu && header) {
+	 menuBtn.addEventListener("click", function () {
+		 this.classList.toggle("active");
+		 navMenu.classList.toggle("open");
+		 document.body.classList.toggle("lock");
+		 document.body.classList.toggle("bg-dark");  // Тогл класу для body
+		 header.classList.toggle("active");
+	 });
+ }
+ 
+ const productAdd = document.querySelector('.product__add');
+ if (productAdd) {
+	 productAdd.addEventListener('click', function () {
+		 this.classList.toggle('active');
+		 document.body.classList.toggle('product-added'); // Тогл іншого класу для body
+	 });
+ }
+ 
